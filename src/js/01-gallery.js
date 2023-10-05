02461f0a04629f494b3f2d23bc6b3bfdff7ee17f
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2023-10-05 15:48:36 +0300

    fix

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+
+const gallery = galleryItems;
+const divGallery = document.querySelector('.gallery');
+
+const markup = gallery.map(({ preview, original, description }) => {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+  </div>`;
+}).join('');
+
+divGallery.insertAdjacentHTML('beforeend', markup);
+
+divGallery.addEventListener('click', urlImage);
+
+function urlImage (e) {
+  e.preventDefault();
+
+  const url = e.target.dataset.source;
+
+  if (!url) return;
+  const instance = basicLightbox.create(`<img src="${url}">`);
+  instance.show();
+}
