c2311dedfa8e15edac4a31dd4bf1c21214fe303f
Oleksii03 <102686871+Oleksii03@users.noreply.github.com>
2023-10-06 12:54:32 +0000

    Deploying to gh-pages from @ Oleksii03/hw-08js@26c9dfb5ca2be737692972c6f15401aff8c054f9 🚀

---
+++ 03-feedback.8a328686.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".feedback-form");e.addEventListener("input",(function(e){t[e.target.name]=e.target.value,localStorage.setItem("feedback-form-state",JSON.stringify(t))}));const t={};console.log(e);
+//# sourceMappingURL=03-feedback.8a328686.js.map
