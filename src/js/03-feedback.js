370a598835ec7d07b27d62dfb1c1ab519d996c22
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2023-10-06 16:04:24 +0300

    f

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,17 +1,3 @@
-// import throttle from "lodash.throttle";
 
-const formEl = document.querySelector('.feedback-form');
 
-formEl.addEventListener('input', onChangeInput);
-
-const feedback = {};
-
-function onChangeInput (e) {
-
-  feedback[e.target.name] = e.target.value;
-
-  localStorage.setItem('feedback-form-state', JSON.stringify(feedback));
-
-}
-
-console.log(formEl);
+console.log('test');
